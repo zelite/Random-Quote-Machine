79716e527e7bdcf0156a6201b93e8b06e6a58975
zelite <jcafernandes@gmail.com>
2016-07-21 16:55:18 +0200

    Use crossorigin proxy

--- js/quotegen.js
+++ js/quotegen.js
@@ -4,19 +4,25 @@ function getNewQuote(){
   //and .quote-author to have the corresponding
   //values on the json
 
+  //callback function that actually updates the DOM
+  function updateQuote(json){
+    $(".quote-text").text(json.quoteText);
+    var author = json.quoteAuthor;
+    if (author === ""){//check if author is empty
+      author= "Unknown";
+    }
+    $(".quote-author").text("-- "+author);
+  }
+
   //makes json request using jsonp method which circunvents cross origin problem
-  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=updateQuote&?callback=?");
+  $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/",
+            {method: "getQuote",
+            format: "json",
+            lang : "en"},
+            updateQuote);
 }
 
-//callback function that actually updates the DOM
-function updateQuote(json){
-  $(".quote-text").text(json.quoteText);
-  var author = json.quoteAuthor;
-  if (author === ""){//check if author is empty
-    author= "Unknown";
-  }
-  $(".quote-author").text("-- "+author);
-}
+
 
 getNewQuote();//Get first quote for page load
 
